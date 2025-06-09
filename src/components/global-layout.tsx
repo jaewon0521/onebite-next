2d35032637c2ee8e3c0b2961693ec3763d766dea
jaewon <>
2025-06-09 12:54:10 +0900

    Feat : Global Layout 작업

---
+++ src/components/global-layout.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function GlobalLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <header>
+        <Link href="/">📚 ONEBITE BOOKS</Link>
+      </header>
+      <main>{children}</main>
+      <footer>제작 @winterlooad</footer>
+    </div>
+  );
+}
