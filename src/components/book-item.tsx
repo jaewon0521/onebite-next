ada6344308ffe5b7991b1b62f1129a4239a38113
jaewon <>
2025-06-09 16:06:33 +0900

    Css : Book List UI

---
+++ src/components/book-item.tsx
@@ -0,0 +1,19 @@
+import { BookData } from "@/type";
+import Link from "next/link";
+import style from "./book-item.module.css";
+
+export default function BookItem({ id, title, subTitle, description, author, publisher, coverImgUrl }: BookData) {
+  return (
+    <Link href={`book/${id}`} className={style.container}>
+      <img src={coverImgUrl} alt={title} />
+      <div>
+        <div className={style.title}>{title}</div>
+        <div className={style.subTitle}>{subTitle}</div>
+        <br />
+        <div className={style.author}>
+          {author} | {publisher}
+        </div>
+      </div>
+    </Link>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
 import SearchableLayout from "@/components/searchable-layout";
-
+import style from "./index.module.css";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 export default function Home() {
-  return <h1>인덱스</h1>;
+  return (
+    <div>
+      <section className={style.container}>
+        <h3>지금 추천하는 도서</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+      <section className={style.container}>
+        <h3>등록된 모든 도서</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+    </div>
+  );
 }
 
 Home.getLayout = (page: React.ReactNode) => {
