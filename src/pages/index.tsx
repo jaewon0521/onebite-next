f85c6503b12e9cf8b98d4cee9debedcd3ab8ca17
jaewon <>
2025-06-09 22:02:36 +0900

    Feat : Mock Data -> Server Side Props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,35 @@
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
-import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
-export default function Home() {
+import { InferGetServerSidePropsType } from "next";
+import { fetchBooks, fetchRandomBooks } from "@/lib/fetch-books";
+
+export const getServerSideProps = async () => {
+  // 컴포넌트보다 먼저 실행되는 함수
+  // return 형식은 Next JS의 문법에 따라 props객체를 포함하는 객체를 반환해야 한다.
+  // Server에서 실행되기 때문에 Browser API를 호출하면 Error가 발생한다.
+
+  const [allBooks, randomBooks] = await Promise.all([fetchBooks(), fetchRandomBooks()]);
+
+  return {
+    props: {
+      allBooks,
+      randomBooks,
+    },
+  };
+};
+export default function Home({ allBooks, randomBooks }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div>
       <section className={style.container}>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
+        {randomBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
       <section className={style.container}>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
+        {allBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,8 +1,21 @@
 import BookItem from "@/components/book-item";
 import SearchableLayout from "@/components/searchable-layout";
-import books from "@/mock/books.json";
+import { fetchBooks } from "@/lib/fetch-books";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 
-export default function SearchPage() {
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+  const q = context.query.q;
+
+  const searchBooks = await fetchBooks(q as string);
+
+  return {
+    props: {
+      books: searchBooks,
+    },
+  };
+};
+
+export default function SearchPage({ books }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div>
       {books.map((book) => (
